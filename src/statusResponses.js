3f7dd9c28d565bdde3644a62b5c16b5951ac75a1
Jack Walsh <jacksonwalsh931@gmail.com>
2023-02-12 20:45:42 -0500

    through all of them

--- src/statusResponses.js
+++ src/statusResponses.js
@@ -34,7 +34,7 @@ const badRequest = (request, response, acceptedTypes, params) => {
   let statusCode;
   let message;
 
-  if (!params.valid || !params.valid === 'true') {
+  if (!params.valid || params.valid !== 'true') {
     message = 'Missing valid query parameter set to true';
     statusCode = 400;
   } else {
@@ -62,7 +62,102 @@ const badRequest = (request, response, acceptedTypes, params) => {
 };
 
 const badRequestMeta = (request, response, acceptedTypes, params) => {
-  respond(request, response, (!params.valid || !params.valid === 'true' ? 400 : 200), acceptedTypes[0]);
+  respond(request, response, (!params.valid || params.valid !== 'true' ? 400 : 200), acceptedTypes[0]);
+};
+
+const unauthorized = (request, response, acceptedTypes, params) => {
+  let statusCode;
+  let message;
+
+  if (!params.loggedIn || params.loggedIn !== 'yes') {
+    message = 'Missing loggedIn query parameter set to yes';
+    statusCode = 401;
+  } else {
+    message = 'This is a valid response.';
+    statusCode = 200;
+  }
+
+  if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response>';
+    responseObj = `${responseObj} <message>${message}</message>`;
+    if (statusCode === 401) {
+      responseObj = `${responseObj} <id>unauthorized</id>`;
+    }
+    responseObj = `${responseObj} </response>`;
+  } else { // must be json / defaulting
+    const responseJSON = {
+      message,
+    };
+    if (statusCode === 401) {
+      responseJSON.id = 'unauthorized';
+    }
+    responseObj = JSON.stringify(responseJSON);
+  }
+  respond(request, response, statusCode, acceptedTypes[0], responseObj);
+};
+
+const unauthorizedMeta = (request, response, acceptedTypes, params) => {
+  respond(request, response, (!params.loggedIn || params.loggedIn !== 'yes' ? 401 : 200), acceptedTypes[0]);
+};
+
+const forbidden = (request, response, acceptedTypes) => {
+  if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response>';
+    responseObj = `${responseObj} <message>You do not have access to this content.</message>`;
+    responseObj = `${responseObj} <id>forbidden</id>`;
+    responseObj = `${responseObj} </response>`;
+  } else { // must be json / defaulting
+    const responseJSON = {
+      message: 'You do not have access to this content',
+      id: 'forbidden',
+    };
+    responseObj = JSON.stringify(responseJSON);
+  }
+  respond(request, response, 403, acceptedTypes[0], responseObj);
+};
+
+const forbiddenMeta = (request, response, acceptedTypes) => {
+  respond(request, response, 403, acceptedTypes[0]);
+};
+
+const internal = (request, response, acceptedTypes) => {
+  if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response>';
+    responseObj = `${responseObj} <message>Internal Server Error. Something went wrong.</message>`;
+    responseObj = `${responseObj} <id>internalError</id>`;
+    responseObj = `${responseObj} </response>`;
+  } else { // must be json / defaulting
+    const responseJSON = {
+      message: 'Internal Server Error. Something went wrong.',
+      id: 'internalError',
+    };
+    responseObj = JSON.stringify(responseJSON);
+  }
+  respond(request, response, 500, acceptedTypes[0], responseObj);
+};
+
+const internalMeta = (request, response, acceptedTypes) => {
+  respond(request, response, 500, acceptedTypes[0]);
+};
+
+const notImplemented = (request, response, acceptedTypes) => {
+  if (acceptedTypes[0] === 'text/xml') {
+    responseObj = '<response>';
+    responseObj = `${responseObj} <message>A get request for this page has not been implemented yet. Check again later for updated content.</message>`;
+    responseObj = `${responseObj} <id>notImplemented</id>`;
+    responseObj = `${responseObj} </response>`;
+  } else { // must be json / defaulting
+    const responseJSON = {
+      message: 'A get request for this page has not been implemented yet. Check again later for updated content.',
+      id: 'notImplemented',
+    };
+    responseObj = JSON.stringify(responseJSON);
+  }
+  respond(request, response, 501, acceptedTypes[0], responseObj);
+};
+
+const notImplementedMeta = (request, response, acceptedTypes) => {
+  respond(request, response, 501, acceptedTypes[0]);
 };
 
 const notFound = (request, response, acceptedTypes) => {
@@ -97,4 +192,12 @@ module.exports = {
   notFoundMeta,
   badRequest,
   badRequestMeta,
+  unauthorized,
+  unauthorizedMeta,
+  forbidden,
+  forbiddenMeta,
+  internal,
+  internalMeta,
+  notImplemented,
+  notImplementedMeta,
 };
